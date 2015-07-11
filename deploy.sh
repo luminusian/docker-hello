e7b6a0e763fdba3f1cd1f76c32f37d7f8d51cf2e
Your Name <you@example.com>
2015-07-11 15:28:40 +0900

    Sets docker hub repo

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,8 @@ SHA1=$1
 EB_BUCKET=elasticbeanstalk-us-west-2-489284558735
 
 # Deploy image to Docker Hub
-docker push circleci/hello:$SHA1
+docker push apingjian/hello:$SHA1
+# docker push circleci/hello:$SHA1
 
 # Create new Elastic Beanstalk version
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
